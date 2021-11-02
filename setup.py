33a9dfd86a7d3f818af6a52f1a59e76c87593f43
hirp7 <63657707+hirp7@users.noreply.github.com>
2021-11-02 23:04:38 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'my_python_package',
+    packages = ['my_python_package'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'hirp7',
+    author_email = '',
+    url = 'https://github.com/hirp7/Maia',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
