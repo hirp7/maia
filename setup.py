aa4c763ac0e57f32b61187fcb3fd7d497e184d4b
hirp7 <63657707+hirp7@users.noreply.github.com>
2021-11-02 23:12:41 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from distutils.core import setup
 
 setup(
-    name = 'my_python_package',
-    packages = ['my_python_package'],
+    name = 'maia',
+    packages = ['maia'],
     version = '0.1',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
+    description = 'Microwave passive filter design assistant tool',
+    #long_description = readme,
     author = 'hirp7',
     author_email = '',
     url = 'https://github.com/hirp7/Maia',
-    download_url = 'download link you saved',
-    keywords = ['tag1', 'tag2'],
+    download_url = '',
+    keywords = ['RF', 'Microwave Engineering','Filter Design'],
     classifiers = [],
+    install_requires = [
+        'numpy',
+        'scipy',
+        'scikit-rf'],
 )
