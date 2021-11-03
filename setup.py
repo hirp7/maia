f6da376069b94adce0a6c9115e8044ff9bb1c8b9
hirp7 <63657707+hirp7@users.noreply.github.com>
2021-11-03 22:28:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from distutils.core import setup
-
+#from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
     name = 'maia',
-    packages = ['maia'],
     version = '0.1',  # Ideally should be same as your GitHub release tag varsion
     description = 'Microwave passive filter design assistant tool',
     #long_description = readme,
     author = 'hirp7',
-    author_email = '',
+    #author_email = '',
+    packages = find_packages(exclude=('maia'))
     url = 'https://github.com/hirp7/Maia',
     download_url = '',
     keywords = ['RF', 'Microwave Engineering','Filter Design'],
     classifiers = [],
+    license = license,
     install_requires = [
         'numpy',
         'scipy',
