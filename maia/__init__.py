84be1ec8b80c87bb19a3bedea475549818348b40
hirp7 <63657707+hirp7@users.noreply.github.com>
2021-11-05 19:38:51 +0100

    Update __init__.py

--- maia/__init__.py
+++ maia/__init__.py
@@ -1,8 +1,1 @@
-import maia,maia_functions
 
-__copyright__    = 'Copyright (C) 2021 hirp7'
-__version__      = '0.0.1'
-__license__      = 'MIT'
-__author__       = 'hirp7'
-__author_email__ = 'izunyan7@gmail.com'
-__url__          = 'http://github.com/account/repository'
