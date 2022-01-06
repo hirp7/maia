274982a20fbb06760518871cacd003c7211f3cc4
Hiro <63657707+hirp7@users.noreply.github.com>
2022-01-06 21:40:52 +0100

    Update classes.py

--- maia/classes.py
+++ maia/classes.py
@@ -30,6 +30,7 @@ c0 = 2.9979e8
 atan = np.arctan
 
 
+
 """
 Filter Analysis
 from the specification such as center frequency, fractional bandwidth, etc...
@@ -175,4 +176,217 @@ class Test1(Test0):
         super().__init__(a,b)
         self.c = c
 """
+
+sqrt = np.sqrt
+arctan = np.arctan
+
+class MatchingCircuit(object):
+    """
+    class MatchingCircuit(Z0,Zl,frequency)
+    input parameters are following:
+        Z0: source impedance, real value
+        Zl: load impedance, complex value
+        frequency: matching frequency
+        
+    method
+        *initial methods
+        .solve_reactance()
+            calculates analytical reactance X and susceptance B of the matching circuit
+            there are two types of circuit, impedance circuit or admittance circuit
+            in accordance with the relation between the load impedance and the source impedance.
+            if the load impedance is larger than source impedance, i.e. located at outside 1+1j circle in a smith chart,
+            impedance circuit is chosen. If not, admittance circuit
+            
+        .find_lumped()
+            determines lumped element value from analytical solutions above
+            generates also schematic class using schemdraw
+            
+        .schematic_LC()
+        .plot_summary()
+        
+        *others
+        .stub()
+            with given line information such as characteristic impedance and propagation constant of microstrip line or waveguide,
+            single-stub matching circuit is constructed
+        .quarter()
+            with given line information such as characteristic impedance and propagation constant of microstrip line or waveguide,
+            a single quarter-wavelength circuit is constructed
+        
+    """
+
+    def __init__(self,Z0,Zl,fc,fbw,npoints):
+        c0 = 2.99792458e8
+        f2beta0 = lambda f:2*pi*f/c0
+        # load Network
+        self.Z0 = Z0
+        self.Frequency = rf.Frequency((fc-fc*fbw/2)/1e9,(fc+fc*fbw/2)/1e9,npoints,unit = 'ghz')
+        self.fc = self.Frequency.center
+        self.media = DefinedGammaZ0(frequency = self.Frequency, z0= self.Z0,gamma = 1j*f2beta0(self.Frequency.f))
+        self.Zl = Zl
+        self.load = self.media.load(rf.zl_2_Gamma0(self.Z0,self.Zl))
+        #media.load(rf.zl_2_Gamma0(100,200-100j)).plot_s_db()
+        self.Rl,self.Xl = [Zl.real,Zl.imag] #lumped element
+        self.XB1,self.XB2,self.circuit_type = self.solve_reactance() #reactance and susceptance
+        self.LC1,self.LC2 = [self.find_lumped(self.XB1),self.find_lumped(self.XB2)]
+        #LC value, flag, Networkcalculated inductance and capacitance to output of Network file
+        self.schematic1 = self.schematic_LC(self.LC1[0],self.LC1[1])
+        self.schematic2 = self.schematic_LC(self.LC2[0],self.LC2[1])
+        self.schematic_size = self.schematic1.get_bbox()
+        self.Network1,self.Network2 = [self.LC1[-1],self.LC2[-1]]
+        self.matched1,self.matched2 = [self.Network1**self.load,self.Network2**self.load]
+        
+        
+    def solve_reactance(self): #LC or CL
+        
+        if self.Rl >= self.Z0:
+            B1 = (self.Xl + sqrt(self.Rl/self.Z0)*sqrt(self.Rl**2 + self.Xl**2 - self.Z0*self.Rl))/(self.Rl**2 + self.Xl**2)
+            B2 = (self.Xl - sqrt(self.Rl/self.Z0)*sqrt(self.Rl**2 + self.Xl**2 - self.Z0*self.Rl))/(self.Rl**2 + self.Xl**2)
+        
+            X1 = 1/B1 + self.Xl*self.Z0/self.Rl - self.Z0/B1/self.Rl
+            X2 = 1/B2 + self.Xl*self.Z0/self.Rl - self.Z0/B2/self.Rl
+            #self.Zm1 = [self.X1,self.B1]
+            #self.Zm2 = [self.X2,self.B2]
+            circuit_type = 'impedance'
+
+            
+            
+        else: #circuit_type == 'Admittance'
+            X1 = sqrt(self.Rl*(self.Z0-self.Rl)) - self.Xl
+            X2 = -sqrt(self.Rl*(self.Z0-self.Rl)) - self.Xl
+            
+            B1 = sqrt((self.Z0 - self.Rl)/self.Rl)/self.Z0
+            B2 = -sqrt((self.Z0 - self.Rl)/self.Rl)/self.Z0
+            circuit_type = 'admittance'        
+            
+        return [X1,B1],[X2,B2],circuit_type
+        
+        
+        
+    def find_lumped(self,XB): #[X, B]
+        if self.circuit_type == 'impedance': #from Zm to LC
+            if XB[0] > 0: #X is inductance otherwise, capacitance                
+                LC = [XB[0]/(2*pi*self.fc),XB[1]/(2*pi*self.fc)]#LC
+                LCflag = True
+                Network = self.media.inductor(LC[0]) ** self.media.shunt_capacitor(LC[1])
+                
+            else:
+                LC = [-1/(XB[0]*2*pi*self.fc),-1/(XB[1]*2*pi*self.fc)]#CL
+                LCflag = False
+                Network = self.media.capacitor(LC[0])**self.media.shunt_inductor(LC[1])
+            
+        if self.circuit_type == 'admittance': #from Ym to LC
+            if XB[0] > 0: #X is inductance otherwise, capacitance                
+                LC = [XB[0]/(2*pi*self.fc),XB[1]/(2*pi*self.fc)]#CL
+                LCflag = True
+                Network = self.media.shunt_capacitor(LC[0])**self.media.inductor(LC[1])
+            else:
+                LC = [-1/(XB[0]*2*pi*self.fc),-1/(XB[1]*2*pi*self.fc)]#LC
+                LCflag = False
+                Network =self.media.shunt_inductor(LC[0])**self.media.capacitor(LC[1])
+        return LC,LCflag,Network
+        
+                
+    def schematic_LC(self,LC,flag):
+        if self.circuit_type == 'impedance':
+            d = schemdraw.Drawing()    
+            Start = d.add(elm.Resistor().label(str(self.Z0) + ' Ohm'))   
+            if flag == True:
+                d += elm.Inductor().label(str(np.round(self.unit_change(LC[0])[0],2)) + self.unit_change(LC[0])[1] +'H') 
+                d.push()
+                d += elm.Capacitor().down().label(str(np.round(self.unit_change(LC[1])[0],2)) + self.unit_change(LC[1])[1] +'F',rotate = True,loc = 'bottom') 
+                d.pop()
+                d += elm.Line()
+                d += elm.Resistor().down().label(str(self.Zl) + ' Ohm',rotate = True,loc = 'bottom')
+                d += elm.Line().left().tox(Start.start)
+            else:
+                d += elm.Capacitor().label(str(np.round(self.unit_change(LC[0])[0],2)) + self.unit_change(LC[0])[1] +'F') 
+                d.push()
+                d += elm.Inductor().down().label(str(np.round(self.unit_change(LC[1])[0],2)) + self.unit_change(LC[1])[1] +'H',rotate = True,loc = 'bottom') 
+                d.pop()
+                d += elm.Line()
+                d += elm.Resistor().down().label(str(self.Zl) + ' Ohm',rotate = True,loc = 'bottom')
+                d += elm.Line().left().tox(Start.start)            
+            
+
+        elif self.circuit_type == 'admittance':
+            d = schemdraw.Drawing()    
+            Start = d.add(elm.Resistor().label(str(self.Z0) + ' Ohm'))   
+            if flag == True:
+                d.push()
+                d += elm.Capacitor().down().label(str(np.round(self.unit_change(LC[0])[0],2)) + self.unit_change(LC[0])[1] +'F',rotate = True,loc = 'bottom') 
+                d.pop()
+                d += elm.Inductor().label(str(np.round(self.unit_change(LC[1])[0],2)) + self.unit_change(LC[1])[1] +'H') 
+                d += elm.Resistor().down().label(str(self.Zl) + ' Ohm',rotate = True,loc = 'bottom')
+                d += elm.Line().left().tox(Start.start)
+            else:
+                d.push()
+                d += elm.Inductor().down().label(str(np.round(self.unit_change(LC[0])[0],2)) + self.unit_change(LC[0])[1] +'H',rotate = True,loc = 'bottom') 
+                d.pop()
+                d += elm.Capacitor().label(str(np.round(self.unit_change(LC[1])[0],2)) + self.unit_change(LC[1])[1] +'F') 
+                d += elm.Resistor().down().label(str(self.Zl) + ' Ohm',rotate = True,loc = 'bottom')
+                d += elm.Line().left().tox(Start.start)
+        return d
+
+    
+    def plot_summary(self,figsize = (8,4),hspace = 0.2,wspace = 0):
+        
+        fig,ax = plt.subplots(1,2,figsize = figsize)
+        fig2,ax2 = plt.subplots(2,1,figsize = (6.4,5.4))
+        fig.subplots_adjust(hspace = hspace,wspace = wspace)
+        ax2[0].axis('off')
+        ax2[1].axis('off')
+        xmin,ymin,xmax,ymax = self.schematic_size
+        self.matched1.plot_s_mag(ax = ax[0],label = 'Sol1') 
+        self.matched2.plot_s_mag(ax = ax[0],label = 'Sol2')
+        self.matched1.plot_s_smith(ax = ax[1],label = 'Sol1') 
+        self.matched2.plot_s_smith(ax = ax[1],label = 'Sol2')
+
+        self.schematic1.draw(ax = ax2[0])
+        self.schematic2.draw(ax = ax2[1])
+        ax2[0].set(xlim=(xmin, xmax), ylim=(ymin, ymax))
+        ax2[1].set(xlim=(xmin, xmax), ylim=(ymin, ymax))
+        fig2.subplots_adjust(hspace = 0.2)
+        
+
+    
+    def unit_change(self,A):
+        unit = None
+        if A >= 1e-18 and A < 1e-15:
+            A = A*1e18
+            unit = 'a'
+        elif A >= 1e-15 and A < 1e-12:
+            A = A*1e15
+            unit = 'f'
+        elif A >= 1e-12 and A < 1e-9:
+            A = A*1e12
+            unit = 'p'
+        elif A >= 1e-9 and A < 1e-6:
+            A = A*1e9
+            unit = 'n'
+        elif A >= 1e-6 and A < 1e-3:
+            A = A*1e6
+            unit = 'u'
+        return A,unit
+        
+        
+
+
+        #def schematic_lumped(self):
+    """
+    def X2LC(self,X):
+        if x > 0: #
+            L_flag = True
+            return X/(2*pi*self.frequency),L_flag
+        else: #C
+            L_flag= False
+            return 1/(2*pi*self.frequency*X),L_flag
+        
+    def B2LC(self,B):
+        if B > 0:
+            C_flag = True
+            return B/(2*pi*self.frequency),C_flag
+        else:
+            C_flag = False
+            return 1/(2*pi*self.frequency*B),C_flag
+    """  
 
